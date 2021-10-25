98f0d05a3bb0fc1b0af015d4549d368d31df843a
Matt Wilkinson <mattwilki17@gmail.com>
2021-10-24 22:16:08 -0400

    yucca and sorting

--- src/main.rs
+++ src/main.rs
@@ -1,65 +1,13 @@
 fn main() {
-    // let graph: Vec<Vec<u32>> = vec![
-    //     vec![0, 1, 1, 0, 1],
-    //     vec![1, 0, 1, 0, 0],
-    //     vec![0, 1, 0, 0, 1],
-    //     vec![1, 1, 1, 0, 1],
-    //     vec![0, 1, 1, 0, 0],
-    // ];
+    let mut set_a = vec![5, 7, 20, 55];
+    let mut set_b = vec![55, 5, 20, 72];
 
-    let graph: Vec<Vec<u32>> = vec![
-        vec![0, 1, 1, 0],
-        vec![1, 0, 1, 1],
-        vec![0, 0, 0, 0],
-        vec![1, 0, 1, 0],
-    ];
+    set_a.sort_unstable();
+    set_b.sort_unstable();
 
-    for (vertex, edges) in graph.iter().enumerate() {
-        let mut is_yucca = true;
-
-        // check if the vertex has n-1 outdegree (has outgoing edges to all other vertexes)
-        for (edge_number, edge_value) in edges.iter().enumerate() {
-            if edge_number == vertex {
-                continue;
-            }
-
-            // println!( vertex {} edge {} is {}", vertex, edge_number, edge_value);
-
-            // we're skipping our own vertex, so if any edge isn't 1, this isn't a yucca
-            if *edge_value != 1 {
-                is_yucca = false;
-                break;
-            }
-        }
-
-        if !is_yucca {
-            continue;
-        }
-
-        // Check if no other vertex has a edge to this vertex
-
-        for other_edges in graph.iter() {
-            let value = other_edges[vertex];
-
-            if value == 1 {
-                is_yucca = false;
-                break;
-            }
-        }
-
-        if !is_yucca {
-            continue;
-        }
-
-        // got to the end, we found a yucca
-
-        println!(
-            "This graph is a YUCCA. Vertex {} has indegree 0 and outdegree {}-1.",
-            vertex,
-            graph.len()
-        );
-        return;
+    if set_a == set_b {
+        println!("Sets are equal")
+    } else {
+        println!("Sets are not equal");
     }
-
-    println!("This graph is not a YUCCA.");
 }
